001c01aa8ef18358dd219a8444ba9c7b27f23e36
RKVanHorn <acinonyx79@hotmail.com>
2023-05-31 10:11:02 -0700

    week6 project

---
+++ week06BE/src/week06finalproject/Deck.java
@@ -0,0 +1,39 @@
+package week06finalproject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class Deck {
+	List<Card> cards = new ArrayList<Card>();
+	
+	public Deck() {
+		String[] names = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+		String[] suits = {"♥", "♦", "♣", "♠"};
+		for(String suit : suits) {
+			for (String name : names) {
+				cards.add(new Card(name, suit, (Arrays.asList(names).indexOf(name)+2)));
+			}
+		}
+	}
+	
+	public void describe() {
+		for (Card card :cards) {
+			card.describe();
+		}
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);		
+	}
+	
+	public Card draw(){
+		Card drawnCard = cards.remove(0);		
+		return drawnCard;
+		
+	}
+}
+
